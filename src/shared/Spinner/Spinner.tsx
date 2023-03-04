b0951e338088c3e13e964fd461d319d47d8bc5cc
Yahir Trejo <yahir.trejo.g@gmail.com>
2023-03-03 21:15:48 -0600

    chore: add spinner component

---
+++ src/shared/Spinner/Spinner.tsx
@@ -0,0 +1,27 @@
+import { SpinnerProps } from '../Interfaces'
+import styles from './spinner.module.css'
+
+const Spinner = ({color = 'var(--color-light)', isLoading, size = 18, ...props}: SpinnerProps) => {
+  return (
+    isLoading ? (
+        <span className={styles.spinnerContainer} {...props}>
+          <svg
+            className={styles.spinner}
+            viewBox={`0 0 ${size} ${size}`}
+            width={`${size}px`}
+            height={`${size}px`}
+          >
+            <circle
+              className={styles.path}
+              cx={`${size / 2}`} cy={`${size / 2}`} r={`${size * .40}`}
+              fill="none"
+              strokeWidth={`${size / 10}`}
+              stroke={color}
+            ></circle>
+          </svg>
+        </span>
+    ) : null
+  )
+}
+
+export default Spinner
