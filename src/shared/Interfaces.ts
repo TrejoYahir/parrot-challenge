a1a5e8a18908f0c4b5171bcc7dbbf35698d49a98
Yahir Trejo <yahir.trejo.g@gmail.com>
2023-03-03 21:16:03 -0600

    chore: add necessary interfaces

--- src/shared/Interfaces.ts
+++ src/shared/Interfaces.ts
@@ -7,3 +7,61 @@ export interface Authentication {
   username: string
   password: string
 }
+
+export interface SpinnerProps {
+  color?: string
+  isLoading: boolean
+  size?: number
+  [x:string]: any
+}
+
+export interface Store {
+  uuid: string
+  name: string
+  availabilityState: string
+  providers: Provider[]
+  secret: string
+  legacyId: string | null
+  organizationUuid: string
+}
+
+export interface Provider {
+  [x:string]: any
+}
+
+export interface User {
+  uuid: string
+  email: string
+  username: string
+  stores: Store[]
+}
+
+export interface UserResponse {
+  status: string
+  result: User
+} 
+
+export interface Product {
+  uuid: string
+  name: string
+  description: string
+  imageUrl: string
+  legacyId: string | null
+  price: string
+  alcoholCount: number
+  soldAlone: boolean
+  availabilityState: string
+  providerAvailability: string | null
+  category: Category
+} 
+
+export interface Category {
+  uuid: string,
+  name: string,
+  sortPosition: number
+}
+
+export interface ProductResponse {
+  status: string
+  results: Product[]
+} 
