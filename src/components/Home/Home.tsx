bcb929800cae1fc6ee3ba68cfe0d801b23d2ab3e
Yahir Trejo <yahir.trejo.g@gmail.com>
2023-03-03 21:18:54 -0600

    chore: add persistence, user and store fetching

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,39 @@
-import { useSelector } from 'react-redux'
-import { selectAccessToken } from '../../features/auth/authSlice'
+import { useEffect, useState } from 'react'
+import { useGetProductsQuery } from '../../features/store/storeApiSlice'
+import { useGetUserQuery } from '../../features/user/userApiSlice'
 import styles from './home.module.css'
 
 const Home = () => {
-  const accessToken = useSelector(selectAccessToken)
+
+  const [selectedStoreId, setSelectedStoreId] = useState<string | null>(null)
+
+  const {
+    data: user,
+    isLoading: userLoading,
+    isSuccess: userSuccess,
+    isError: userHasError,
+    error: userError,
+  } = useGetUserQuery()
+
+  const {
+    data: products,
+    isLoading: productsLoading,
+    isSuccess: productsSuccess,
+    isError: productsHasError,
+    error: productsError,
+  } = useGetProductsQuery(selectedStoreId || '', { skip: !selectedStoreId })
+
+  useEffect(() => {
+    if (userSuccess && user?.stores[0]?.uuid) {
+      setSelectedStoreId(user.stores[0].uuid)
+    }
+  }, [userSuccess, user])
+  
   return (
-    <div>{accessToken}</div>
+    <section>
+      <div>{user?.username}</div>
+      <div>{ products?.map((product) => <div key={product.uuid}>{ product.name }</div>) }</div>
+    </section>
   )
 }
 
--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -3,8 +3,8 @@ import type { RootState } from '../../app/store'
 import { Credentials } from '../../shared/Interfaces'
 
 const initialState: Credentials = {
-  accessToken: null,
-  refreshToken: null
+  accessToken:  localStorage.getItem('accessToken') || null,
+  refreshToken: localStorage.getItem('refreshToken') || null,
 }
 
 const authSlice = createSlice({
@@ -15,10 +15,13 @@ const authSlice = createSlice({
       const { access, refresh } = action.payload
       state.accessToken = access
       state.refreshToken = refresh
+      localStorage.setItem('accessToken', access)
+      localStorage.setItem('refreshToken', refresh)
     },
     logOut: (state) => {
       state.accessToken = null
       state.refreshToken = null
+      localStorage.clear()
     },
   },
 })
